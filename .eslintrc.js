94d20a7edaef6d5746ce796b0303d29d87ce707d
BogdanPasterak <bogdanpasterak@gmail.com>
2018-04-09 23:08:16 +0100

    fead: Solution

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "plugins": ["jasmine"],
+    "extends": ["eslint:recommended", "plugin:jasmine/recommended"],
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "jquery": true,
+        "jasmine": true
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "globals": {
+        "allFeeds": true,
+        "loadFeed": true,
+        "var2": false
+    }
+};
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,46 +27,134 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* HACK: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        allFeeds.forEach(function(feed) {
+            it('each feed has a URL and that is not empty', function() {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            });
+        });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* HACK: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        allFeeds.forEach(function(feed) {
+            it('each feed has a name and that is not empty', function() {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            });
+        });
+
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* HACK: Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
+        /* HACK: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('the menu element is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        /* HACK: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+        it('the menu element is visibility after clicking the menu icon', function() {
+            $('.menu-icon-link').click();
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+            expect($('body').hasClass('menu-hidden')).not.toBe(true);
 
-        /* TODO: Write a test that ensures when the loadFeed
+            $('.menu-icon-link').click();
+
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+
+    });
+
+    /* HACK: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+        let length;
+        /* HACK: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+        afterEach(function() {
+            // is an elegant form of sending your own message, but requires installation
+            // 'jasmine2-custom-message' (https://github.com/avrelian/jasmine2-custom-message)
+            console.log(length);
+        });
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        it('the .feed has at least one .entry-link', function(done) {
+            length = $('.feed .entry-link').length;
+
+            expect(length).toBeGreaterThan(0);
+            done();
+        });
+
+    });
+
+    /* HACK: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+        const title = $('.header-title');
+        let index;
+        let header;
+        /* HACK: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        beforeEach(function(done) {
+
+            // if (allFeeds.some(feed => feed.name === title.text())) {
+            // if this will be the first test title.text () === 'Feeds' (from index.html)
+            // index will take -1. And great, next will be 0, first in the array !!!
+            index = allFeeds.indexOf(allFeeds.filter(feed => feed.name === title.text())[0]);
+            //}
+            // next in allFeeead array
+            index++;
+            // if last one -> start from 0
+            if (index >= allFeeds.length) {
+                index = 0;
+            }
+            console.log(title.text() + '  ' + index );
+            // before call header is ...
+            header = title.text();
+
+            loadFeed(index, function() {
+                done();
+            });
+        });
+
+        afterEach(function() {
+
+            index = allFeeds.indexOf(allFeeds.filter(feed => feed.name === title.text())[0]);
+            // after call header is ...
+            console.log(title.text() + '  ' + index );
+        });
+
+        it('new feed is loaded that the content actually changes.', function(done) {
+            // I expects different titles after calling the function
+            expect(title.text()).not.toBe(header);
+            done();
+        });
+
+
+    });
+
 }());
